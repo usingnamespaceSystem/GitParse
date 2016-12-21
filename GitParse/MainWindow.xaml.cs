00f0bdadb9c492e99631547637eb381430e60d1f
usingnamespaceSystem <faust_32@inbox.ru>
2016-12-21 18:34:50 +0300

    fix of 403 issues and missed profile params exeption

--- GitParse/MainWindow.xaml.cs
+++ GitParse/MainWindow.xaml.cs
@@ -28,12 +28,21 @@ namespace GitParse
 
         protected string Get_repos(string url)
         {
-            HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
-            request.UserAgent = "GIT_Parse";
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            try
             {
-                StreamReader reader = new StreamReader(response.GetResponseStream());
-                return reader.ReadToEnd();
+
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.UserAgent = "GIT_Parse";
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    return reader.ReadToEnd();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Too much requests per hour. Try again later.");
+                return String.Empty;
             }
         }
 
@@ -41,10 +50,13 @@ namespace GitParse
         {
             string json = Get_repos("https://api.github.com/repositories?client_id=c24d271954910f698d45&client_secret=ca09cf626abf98c9bc39378379a6964807e0563c");
 
-            data = JsonMapper.ToObject(json);
+            if (json != String.Empty)
+            {
+                data = JsonMapper.ToObject(json);
 
-            foreach (JsonData jsd in data)
-                listView.Items.Add(jsd["owner"]["login"].ToString());
+                foreach (JsonData jsd in data)
+                    listView.Items.Add(jsd["owner"]["login"].ToString());
+            }
         }
 
         private void listView_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -73,12 +85,31 @@ namespace GitParse
                     if (jsd["owner"]["login"].ToString() == listView.SelectedItem.ToString())
                     {
                         string prof = Get_repos(jsd["owner"]["url"].ToString());
-                        JsonData profile_data = JsonMapper.ToObject(prof);
 
-                        list.Items.Add("Name: " +profile_data["name"].ToString());
-                        list.Items.Add("E-mail: " + profile_data["email"].ToString());
-                        list.Items.Add("Created: " + profile_data["created_at"].ToString());
-                        list.Items.Add("Location: " + profile_data["location"].ToString()); break;
+                        if (prof != String.Empty)
+                        {
+                            JsonData profile_data = JsonMapper.ToObject(prof);
+
+                            if (profile_data["name"] != null)
+                                list.Items.Add("Name: " + profile_data["name"].ToString());
+                            else
+                                list.Items.Add("Name: ");
+
+                            if (profile_data["email"] != null)
+                                list.Items.Add("E-mail: " + profile_data["email"].ToString());
+                            else
+                                list.Items.Add("E-mail: ");
+
+                            if (profile_data["created_at"] != null)
+                                list.Items.Add("Created: " + profile_data["created_at"].ToString());
+                            else
+                                list.Items.Add("Created: ");
+
+                            if (profile_data["location"] != null)
+                                list.Items.Add("Location: " + profile_data["location"].ToString());
+                            else
+                            break;
+                        }
                     }
                 }
 
